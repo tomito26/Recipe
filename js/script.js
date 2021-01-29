1b5524f4b55829493f8696d81954944392083a1b
tomito26 <tommybwah@gmail.com>
2021-01-29 09:33:54 +0300

    added dom for display the random meal from the api

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ async function getRandomMeal(){
     const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const respData = await resp.json();
     const randomMeal = respData.meals[0];
+    console.log(randomMeal)
     displayRandomMeal(randomMeal,true);
 }
 async function getMealById(id){
@@ -14,7 +15,23 @@ async function getMealsBySearch(term){
     const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
 }
 
-function displayRandomMeal(){
-    
+function displayRandomMeal(mealData,random = false){
+    const meal = document.createElement("div");
+    meal.className="meal";
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${random ?` 
+            <span class="random">Random Recipe</span>`
+            : ""}
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn" onClick=" "><i class="fas fa-heart"></i></button>
+        </div>
+ `;
+    mealContainer.appendChild(meal)
+
+
 }
 
