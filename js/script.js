69eeb780ab3021a0b132dd1fa8c5a28474288275
tomito26 <tommybwah@gmail.com>
2021-01-29 08:49:50 +0300

    added ansychronous function for fetching data from the mealapi

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const mealContainer = document.getElementById("meals")
+
+getRandomMeal()
+async function getRandomMeal(){
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+    displayRandomMeal(randomMeal,true);
+}
+async function getMealById(id){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+async function getMealsBySearch(term){
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
+
+function displayRandomMeal(){
+    
+}
+
